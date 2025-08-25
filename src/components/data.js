0aed9cdd0e4c02c8f716153650466cb3ec89c0ad
njflash88 <gordonchu88@yahoo.com>
2025-08-25 10:24:14 +0800

    first

---
+++ src/components/data.js
@@ -0,0 +1,18 @@
+// import tour1 from "../assets/tour-1.jpeg";
+// import tour2 from "../assets/tour-2.webp";
+// import tour3 from "../assets/tour-3.jpeg";
+// import tour4 from "../assets/tour-4.jpeg";
+// import tour5 from "../assets/tour-4.jpeg";
+
+export const socialLinks = [
+  { id: 1, href: "http://facebook.com", icon: "fa-brands fa-facebook" },
+  { id: 2, href: "http://instagram.com", icon: "fa-brands fa-instagram" },
+  { id: 3, href: "http://twitter.com", icon: "fa-brands fa-square-x-twitter" },
+];
+
+export const pageLinks = [
+  { id: 1, href: "index.html", data: "Home" },
+  { id: 2, href: "#about", data: "Home" },
+  { id: 3, href: "#services", data: "Services" },
+  { id: 4, href: "#tours", data: "Tours" },
+];
