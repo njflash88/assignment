a5ec8e64266a43a27028eae393eb1be69bc81811
njflash88 <gordonchu88@yahoo.com>
2025-08-30 00:32:01 +0800

    France photo link update2

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -4,7 +4,7 @@ import {  Link } from 'react-router-dom';
 // Destinations Page Component
 const Destinations = () => {
    const countries = [
-     { id: 1, name: 'France', image: '../src/assets/France.jpeg' },
+     { id: 1, name: 'France', image: '/src/assets/France.jpeg' },
      { id: 2, name: 'United Kingdom', image: 'https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80' },
      { id: 3, name: 'Germany', image: 'https://images.unsplash.com/photo-1467269204594-9661b134dd2b?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80' },
      { id: 4, name: 'Italy', image: 'https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80' }
