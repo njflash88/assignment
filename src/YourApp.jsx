bdffffcf3f1066b059df14821b267f22a6555de8
njflash88 <gordonchu88@yahoo.com>
2025-08-30 00:26:58 +0800

    France photo link update

---
+++ src/YourApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Try editing me please! 🎉</Text>
+    </View>
+  );
+};
+
+export default YourApp;
